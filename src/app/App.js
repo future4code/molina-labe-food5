eddc9b48546e6a03695607382118b1c0cedadb3d
jaqueline lourenco <jaquelineloure50@gmail.com>
2021-08-03 16:17:49 -0300

    Resposividade e centralizando loading, login e cadastro

--- src/app/App.js
+++ src/app/App.js
@@ -1,29 +1,30 @@
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Router from '../routes/Router';
-
 /*Contexts*/
 import GlobalState from "../global/GlobalState";
-
+import { GlobalStyled } from '../constants/styledGlobal'
 /*Tema*/
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from '../constants/theme';
-
-import React from 'react';
+/*Components*/
 import Header from '../components/Header';
 import FooterNav from '../components/FooterNav';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalState>
-        <BrowserRouter>
-          <Route exact path={['/cadastro', '/cadastro/endereco', '/home', '/carrinho', '/perfil']}>
-            <Header />   
-          </Route>       
-          <Router />
-          <FooterNav/>
-        </BrowserRouter>
-      </GlobalState>
+      <GlobalStyled>
+        <GlobalState>
+          <BrowserRouter>
+            <Route exact path={['/cadastro', '/cadastro/endereco', '/home', '/carrinho', '/perfil']}>
+              <Header />   
+            </Route>       
+            <Router />
+            <FooterNav/>
+          </BrowserRouter>
+        </GlobalState>
+      </GlobalStyled>
     </ThemeProvider>
   );
 }
